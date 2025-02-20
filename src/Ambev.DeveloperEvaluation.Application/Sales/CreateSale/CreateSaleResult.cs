7e809ff76ba9d4e3fbeb0591fd17af95b49b6985
José Seraphim <curcif@live.com>
2025-02-20 14:41:13 -0300

    Getsales

--- src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleResult.cs
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleResult.cs
@@ -3,9 +3,5 @@
     public class CreateSaleResult
     {
         public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public string Customer { get; set; } = string.Empty;
-        public string Branch { get; set; } = string.Empty;
-        public decimal TotalAmount { get; set; }
     }
 }
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale
+{
+    public class GetSaleResult
+    {
+        /// <summary>
+        /// Sale's date
+        /// </summary>
+        public DateTime? Date { get; set; }
+        /// <summary>
+        /// Customer's name
+        /// </summary>
+        public string Customer { get; set; } = string.Empty;
+        /// <summary>
+        /// Tthe total sale amount
+        /// </summary>
+        public decimal? Total { get; set; }
+        /// <summary>
+        /// The store where the buy was made
+        /// </summary>
+        public string Branch { get; set; } = string.Empty;
+        /// <summary>
+        /// The products bought
+        /// </summary>
+        public string Products { get; set; } = string.Empty;
+        /// <summary>
+        /// The quantity of items in the bought
+        /// </summary>
+        public int? Quantities { get; set; }
+    }
+}
