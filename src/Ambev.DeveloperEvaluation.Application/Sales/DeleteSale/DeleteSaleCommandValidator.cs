4f510a2b6e2a4b6f9738d87fa05aa613d730c51a
José Seraphim <curcif@live.com>
2025-02-20 15:00:18 -0300

    Delete

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale
+{
+    /// <summary>
+    /// Validator for DeleteSaleCommand
+    /// </summary>
+    internal class DeleteSaleCommandValidator : AbstractValidator<DeleteSaleCommand>
+    {
+        /// <summary>
+        /// Initializes validation rules for DeleteSaleCommand
+        /// </summary>
+        public DeleteSaleCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Sale ID is required");
+        }
+    }
+}
